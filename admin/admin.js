4f7b1d4572469645c92574bb702c4846c3269123
qxbthree <qxbthree@gmail.com>
2025-05-14 01:05:11 +0800

    done

--- admin/admin.js
+++ admin/admin.js
@@ -31,61 +31,149 @@ feedbacksBtn.addEventListener('click', () => {
 
 async function populateMonitorGraphs() {
   await errorRateGraph()
+  await commonDialectsGraph()
+  await usersOvertimeGraph()
 }
 
 async function errorRateGraph() {
   const errorRateCtx = document.getElementById('error-rate')
 
-  const data = [
-      { dialect: 'tagalog', total: 5, errors: 1 },
-      { dialect: 'cebuano', total: 4, errors: 1 },
-      { dialect: 'ilocano', total: 4, errors: 1 },
-      { dialect: 'hiligayon', total: 3, errors: 1 },
-      { dialect: 'bicolano', total: 2, errors: 1 },
-      { dialect: 'waray', total: 2, errors: 1 },
-      { dialect: 'kapampangan', total: 2, errors: 0 },
-      { dialect: 'pangasinan', total: 2, errors: 1 }
-    ];
+  const errRateRes = await fetch('http://localhost:4321/error-rate')
+  const data = await errRateRes.json()
 
-    const labels = data.map(row => row.dialect);
-    const errorRates = data.map(row => +(row.errors / row.total * 100).toFixed(2)); // %
+  const labels = data.map(row => row.dialect);
+  const errorRates = data.map(row => +(row.errors / row.total * 100).toFixed(2)); // %
 
-    new Chart(errorRateCtx, {
-      type: 'bar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Error Rate (%)',
-          data: errorRates,
-          backgroundColor: 'rgba(255, 99, 132, 0.6)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            max: 100,
-            title: {
-              display: true,
-              text: 'Error Rate (%)'
-            }
-          }
-        },
-        plugins: {
+  new Chart(errorRateCtx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Error Rate (%)',
+        data: errorRates,
+        backgroundColor: 'rgba(255, 99, 132, 0.6)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+          max: 100,
           title: {
             display: true,
-            text: 'Translation Error Rate by Dialect'
-          },
-          tooltip: {
-            callbacks: {
-              label: ctx => `${ctx.raw}% errors`
-            }
+            text: 'Error Rate (%)'
+          }
+        }
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: 'Translation Error Rate by Dialect'
+        },
+        tooltip: {
+          callbacks: {
+            label: ctx => `${ctx.raw}% errors`
           }
         }
       }
-    })
+    }
+  })
+}
+
+async function commonDialectsGraph() {
+  const commonDialectsCtx = document.getElementById('common-dialects')
+
+  const resp = await fetch('http://localhost:4321/common-dialects')
+  const data = await resp.json()
+
+  new Chart(commonDialectsCtx, {
+    type: 'pie',
+    data: {
+      labels: data.map(v => v.dialect),
+      datasets: [{
+        label: 'Most Common Dialects',
+        data: data.map(v => v.count),
+        backgroundColor: [
+          '#ff6384', '#36a2eb', '#cc65fe', '#ffce56',
+          '#4bc0c0', '#9966ff', '#ff9f40', '#c9cbcf'
+        ]
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      plugins: {
+        legend: {
+          position: 'bottom'
+        },
+        title: {
+          display: true,
+          text: 'Most Common Dialects'
+        }
+      }
+    }
+  })
+}
+
+async function usersOvertimeGraph() {
+  const userOvertimeCtx = document.getElementById('user-overtime')
+
+  const resp = await fetch('http://localhost:4321/user-overtime')
+  const data = await resp.json()
+
+  const dates = data.map(v => new Date(v.date))
+  const totalUsers = data.map(v => v.total_users)
+
+  new Chart(userOvertimeCtx, {
+    type: 'line',
+    data: {
+      labels: dates,
+      datasets: [{
+        label: 'New users overtime',
+        data: totalUsers,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1,
+        fill: false,
+        pointRadius: 5,
+        pointBackgroundColor: 'rgb(75, 192, 192)',
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day',
+            tooltipFormat: 'yyyy-MM-dd',
+          },
+          title: {
+            display: true,
+            text: 'Date'
+          }
+        },
+        y: {
+          beginAtZero: true,
+          stepSize: 1,
+          title: {
+            display: true,
+            text: 'Users'
+          }
+        }
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: 'New users'
+        }
+      }
+    }
+  })
 }
 
 async function populateFeedbacks() {
