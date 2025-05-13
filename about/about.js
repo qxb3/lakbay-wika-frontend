a3de21af23baccdfcb77d0c65e39fb1c937049ae
qxbthree <qxbthree@gmail.com>
2025-05-13 21:01:47 +0800

    send request feedback

--- about/about.js
+++ about/about.js
@@ -1,22 +1,45 @@
-const email = document.getElementById('email')
-const body = document.getElementById('body')
+const nameInput = document.getElementById('name')
+const feedbackInput = document.getElementById('feedback')
 const sendBtn = document.getElementById('send-btn')
 
-sendBtn.addEventListener('click', () => {
-  if (!email.value) {
-    sendBtn.textContent = 'Empty email'
+sendBtn.addEventListener('click', async () => {
+  const name = nameInput.value
+  const feedback = feedbackInput.value
+
+  if (!name) {
+    sendBtn.textContent = 'Please input your name'
     sendBtn.style.backgroundColor = 'var(--err)'
     return
   }
 
-  if (!body.value) {
-    sendBtn.textContent = 'Empty feedback body'
+  if (!feedback) {
+    sendBtn.textContent = 'Please input your feedback'
     sendBtn.style.backgroundColor = 'var(--err)'
     return
   }
 
-  const e = encodeURIComponent(email.value)
-  const s = encodeURIComponent('Feedback')
-  const b = encodeURIComponent(body.value)
-  window.location.href = `mailto:ouremail@example.com?subject=${s}&body=Emailer: ${e} - ${b}`;
+  const userId = localStorage.getItem('user')
+  if (!userId) {
+    sendBtn.textContent = 'Please reload the webpage'
+    sendBtn.style.backgroundColor = 'var(--err)'
+    return
+  }
+
+  const feedbackRes = await fetch(`http://localhost:4321/feedback?userid=${encodeURIComponent(userId)}&name=${encodeURIComponent(name)}&feedback=${encodeURIComponent(feedback)}`, {
+    method: 'POST'
+  })
+
+  const result = await feedbackRes.text()
+
+  if (feedbackRes.status !== 200) {
+    sendBtn.textContent = result
+    sendBtn.style.backgroundColor = 'var(--err)'
+    return
+  }
+
+  nameInput.value = ''
+  feedbackInput.value = ''
+
+  sendBtn.textContent = 'Success'
+  sendBtn.style.backgroundColor = 'var(--color2d)'
 })
